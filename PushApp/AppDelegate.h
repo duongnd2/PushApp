7a13dac3fd0c3b6f0733baf8559226510ee2701f
duongnd776 <duongnd@dramall.com.vn>
2014-02-10 16:05:05 +0700

    Initial Commit

---
+++ PushApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PushApp
+//
+//  Created by DuongND on 2/10/14.
+//  Copyright (c) 2014 DuongNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PushApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PushApp
+//
+//  Created by DuongND on 2/10/14.
+//  Copyright (c) 2014 DuongNguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
